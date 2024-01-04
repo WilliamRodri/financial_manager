26ab10758afcb2054248cffd58bd281d4e71a448
William Rodrigues <williamrodrigues.sousa@hotmail.com>
2024-01-04 14:45:45 -0300

    Crud expenses finish

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,4 +1,5 @@
+import { ExpenseModule } from './expense/expense.module';
 import { IncomeModule } from './income/income.module';
 import { UsersModule } from './users/users.module';
 
-export const featureModules = [UsersModule, IncomeModule];
+export const featureModules = [UsersModule, IncomeModule, ExpenseModule];
